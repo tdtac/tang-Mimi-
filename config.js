080c77cf87825e36edae77dd5b008f3085f0e884
tdtac <89760533+tdtac@users.noreply.github.com>
2021-08-30 11:05:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
 const CONFIG = {
     introTitle: 'Cục vàng mì mì ơiiiii!',
     introDesc: `Ngoài trời thì dịch và mưa 
-    Tay anh thì ấm đợi hết dịch a nắm cùng em 
-    Tay a ngắn nhưng nắm e là đủ 
-    a viết nhạc a k viết văn nên nên a hủm giống Tô Hoài 
+    Tay anh thì ấm đợi hết dịch a nắm tay em  
+    A trân trọng mọi thứ về em từ nụ cười tới mái tóc
+    Nên việc e cần làm là luôn cười thay vì khóc 
+    A viết nhạc a k viết văn nên nên a hủm giống Tô Hoài 
     iu e nhớ nhớ dài đêm thì có gì saiiiiiiiiiii`,
     btnIntro: 'YÊU EMMMMMMM',
     title: 'Chúc mừng sinh nhật mì mì của a 😙',
     desc: 'A thương bé càng lúc càng nhiêu ',
     btnYes: 'thương a quài e nhaaa <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không nha, Anh trai :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì thương  <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'qua dịch a dẫn công chúa đi ăn bún bò, ún tà tùa nha công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/dungtracran' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/dungtractran' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
+    
