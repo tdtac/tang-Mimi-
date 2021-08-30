1502f4d667dbfe14c540e9470c2e5779ecd632f2
tdtac <89760533+tdtac@users.noreply.github.com>
2021-08-30 10:52:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'Cục vàng mì mì ơiiiii!',
+    introDesc: `Ngoài trời thì dịch và mưa 
+    Tay anh thì ấm đợi hết dịch a nắm cùng em 
+    Tay a ngắn nhưng nắm e là đủ 
+    a viết nhạc a k viết văn nên nên a hủm giống Tô Hoài 
+    iu e nhớ nhớ dài đêm thì có gì saiiiiiiiiiii`,
+    btnIntro: 'YÊU EMMMMMMM',
+    title: 'Chúc mừng sinh nhật mì mì của a 😙',
+    desc: 'A thương bé càng lúc càng nhiêu ',
+    btnYes: 'thương a quài e nhaaa <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì thương  <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'qua dịch a dẫn công chúa đi ăn bún bò, ún tà tùa nha công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/dungtractran' 
 }
